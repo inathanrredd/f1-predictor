79c7d45242c71010c06fb4855decce15637e25f0
Nathan Redd <nathan.redd@law.byu.edu>
2024-06-20 19:34:06 -0600

    Create predictor.

---
+++ src/App.jsx
@@ -0,0 +1,139 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [predictedResults, setPredictedResults] = useState([])
+  const [round, setRound] = useState(1)
+  const [races, setRaces] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    getRacesThisSeason()
+  }, [])
+  
+  const predictResults = async () => {
+    setLoading(true)
+    let driverScores = []
+    const drivers = await getCurrentDrivers()
+    const driverObjects = {}
+    drivers.forEach((driverObject => driverObjects[driverObject.driverId] = driverObject))
+    const race =  await getRaceFromRoundThisSeason(round)
+    for (const driver of drivers) {
+      const driverId = driver.driverId
+      const currrentSeasonResults = await getCurrentSeasonResults(driverId)
+      const prevResultsAtCircuit = await getPrevResultsAtCircuit(race.Circuit.circuitId, driverId)
+      driverScores.push({ driver: driverObjects[driverId], score: currrentSeasonResults + prevResultsAtCircuit })
+    }
+    driverScores = driverScores.sort((a, b) => { return a.score - b.score })
+    setPredictedResults(driverScores.map(obj => obj.driver.givenName + ' ' + obj.driver.familyName))
+    setLoading(false)
+  }
+
+  const getCurrentSeasonResults = (driver) => {
+    const res = parseInt(sessionStorage.getItem(`${driver}-curr`))
+    return res
+      ? res
+      : axios.get(`http://ergast.com/api/f1/current/drivers/${driver}/results.json`)
+          .then((results) => {
+            const averageResult = getAverageResults(results.data)
+            sessionStorage.setItem(`${driver}-curr`, averageResult)
+            return averageResult
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+  }
+
+  const getPrevResultsAtCircuit = (circuit, driver) => {
+    const res = parseInt(sessionStorage.getItem(`${driver}-${circuit}`))
+    return res
+      ? res
+      : axios.get(`http://ergast.com/api/f1/circuits/${circuit}/drivers/${driver}/results.json`)
+          .then((results) => {
+            const averageResult = getAverageResults(results.data)
+            sessionStorage.setItem(`${driver}-${circuit}`, averageResult)
+            return averageResult
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+  }
+
+  const getAverageResults = (results) => {
+    results = results?.MRData?.RaceTable?.Races
+    if (!results) return 999
+    let total = 0
+    for (const result of results) {
+      total += parseInt(result.Results[0].position)
+    }
+    return total / results.length
+  }
+
+  const getCurrentDrivers = () => {
+    return axios.get('https://ergast.com/api/f1/current/1/drivers.json')
+      .then((results) => {
+        return results?.data?.MRData?.DriverTable?.Drivers
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const getRaceFromRoundThisSeason = (round) => {
+    return axios.get(`http://ergast.com/api/f1/current/${round}.json`)
+      .then((results) => {
+        return results?.data?.MRData?.RaceTable?.Races[0]
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const getRacesThisSeason = () => {
+    return axios.get(`http://ergast.com/api/f1/current.json`)
+      .then((results) => {
+        setRaces(results?.data?.MRData?.RaceTable?.Races)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <>
+      <h1>F1 {new Date().getFullYear()} Predictor</h1>
+      <select
+        value={round}
+        onChange={e => setRound(e.target.value)}
+        style={{ height: "2rem" }}
+      >
+        {
+          races?.map(race => (
+            <option key={race.round} value={parseInt(race.round)}>{race.raceName}</option>
+          ))
+        }
+      </select>
+      <button style={{ marginLeft: "0.5rem" }} onClick={() => predictResults() }>
+        Predict!
+      </button>
+      {loading ? (
+        <>
+          <h3>Predicting Results</h3>
+          <progress value={null} />
+        </>
+      ) : (
+        <>
+          <h3>Predicted Results</h3>
+          <ol>
+            {predictedResults.map((res) => (
+              <li key={res}>{res}</li>
+            ))}
+          </ol>
+        </>
+      )}
+    </>
+  )
+}
+
+export default App
